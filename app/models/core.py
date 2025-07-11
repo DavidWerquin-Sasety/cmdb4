330029a05c88a15b9d1490ffac0847da55bd51a6
david <dwerquin@sasety.com>
2025-07-11 19:37:53 +0200

    v24 OK todo : Authentification Formulaire client

--- app/models/core.py
+++ app/models/core.py
@@ -15,12 +15,20 @@ class SiteType(db.Model):
 class Site(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey("client.id"), nullable=False)
+
     name = db.Column(db.String(128), nullable=False)
     code = db.Column(db.String(32), nullable=False)
     street = db.Column(db.String(128))
     postal_code = db.Column(db.String(16))
     city = db.Column(db.String(64))
     country = db.Column(db.String(64))
+
     site_type_id = db.Column(db.Integer, db.ForeignKey("site_type.id"))
     site_type = db.relationship("SiteType", lazy="joined")
+
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Child collections
+    accesses = db.relationship("InternetAccess", backref="site", cascade="all, delete-orphan")
+    licenses = db.relationship("SaseLicense", backref="site", cascade="all, delete-orphan")
+    equipments = db.relationship("SaseEquipment", backref="site", cascade="all, delete-orphan")
